9f6aa13f036c27f8e46768b04757d23544868fe6
nonoroazoro <xiaochao.k@gmail.com>
2016-11-17 13:06:58 +0800

    update to webpack 2

--- client/demo/index.jsx
+++ client/demo/index.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 
 render(
-    <div class="demo" />,
+    <div className="demo" />,
     document.getElementById("main")
 );
---
+++ webpack.prod.config.js
@@ -0,0 +1,42 @@
+﻿const webpack = require("webpack");
+const config = require("./webpack.base.config");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+config.output.filename = "[name].[chunkhash:8].js";
+config.output.chunkFilename = "[id].[chunkhash:8].chunk.js";
+
+config.module.rules.push(
+    {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+            loader: "css-loader",
+            fallbackLoader: "style-loader"
+        })
+    },
+    {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract({
+            loader: ["css-loader", "less-loader"],
+            fallbackLoader: "style-loader"
+        })
+    }
+);
+
+config.plugins.push(
+    new ExtractTextPlugin("res/[name].[contenthash:8].css"),
+    new webpack.DefinePlugin({
+        "process.env":
+        {
+            NODE_ENV: JSON.stringify("production")
+        }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+        compress:
+        {
+            warnings: false
+        }
+    })
+);
+
+module.exports = config;
