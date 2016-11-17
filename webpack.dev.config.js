e28dc52762ad628d3dad55bfc61fb4aea793aef4
nonoroazoro <xiaochao.k@gmail.com>
2016-11-17 12:53:34 +0800

    update to webpack 2

---
+++ webpack.dev.config.js
@@ -0,0 +1,38 @@
+﻿const webpack = require("webpack");
+const config = require("./webpack.base.config");
+
+config.module.rules.push(
+    {
+        test: /\.jsx?$/,
+        use: ["eslint-loader"],
+        exclude: /node_modules/,
+        enforce: "pre"
+    },
+    {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+    },
+    {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"]
+    }
+);
+
+config.plugins.push(
+    new webpack.SourceMapDevToolPlugin({
+        filename: "[file].map",
+        exclude: ["vendor.js"]
+    })
+);
+
+// HMR.
+// config.module.loaders[0].loaders.unshift("react-hot");
+// for (const key of Object.keys(config.entry))
+// {
+//     config.entry[key].push("webpack-hot-middleware/client?reload=true");
+// }
+// config.plugins.push(
+//     new webpack.HotModuleReplacementPlugin()
+// );
+
+module.exports = config;
