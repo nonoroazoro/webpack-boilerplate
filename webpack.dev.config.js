9290039d6dadf7f113b2fff2bf5a944a72912d8f
nonoroazoro <xiaochao.k@gmail.com>
2016-12-06 15:25:48 +0800

    update to css-modules

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -10,11 +10,29 @@ config.module.rules.push(
     },
     {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", "css-loader"],
+        include: /node_modules/
     },
     {
         test: /\.less$/,
-        use: ["style-loader", "css-loader", "less-loader"]
+        use: ["style-loader", "css-loader", "less-loader"],
+        include: /node_modules/
+    },
+    {
+        test: /\.less$/,
+        use: [
+            "style-loader",
+            {
+                loader: "css-loader",
+                options: {
+                    modules: true,
+                    sourceMap: true,
+                    localIdentName: "[name]-[local]"
+                }
+            },
+            "less-loader"
+        ],
+        exclude: /node_modules/
     }
 );
 
