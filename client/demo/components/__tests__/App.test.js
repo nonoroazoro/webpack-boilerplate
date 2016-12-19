d2e02b20c7fd8811a3008685c0b62260a1fe5b05
nonoroazoro <xiaochao.k@gmail.com>
2016-12-19 16:40:39 +0800

    add enyme

--- client/demo/components/__tests__/App.test.js
+++ client/demo/components/__tests__/App.test.js
@@ -1,10 +1,26 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 
 import App from "../App";
 
 test("App renders correctly.", () =>
 {
-    const wrapper = renderer.create(<App />).toJSON();
+    const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
+});
+
+test("App renders correctly when className is set.", () =>
+{
+    const wrapper = shallow(<App className="home" />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test("App changes the text of button after click.", () =>
+{
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("button").text()).toEqual("Click: 0");
+
+    wrapper.find("button").simulate("click");
+    expect(wrapper.state("clicked")).toEqual(1);
+    expect(wrapper.find("button").text()).toEqual("Click: 1");
 });
